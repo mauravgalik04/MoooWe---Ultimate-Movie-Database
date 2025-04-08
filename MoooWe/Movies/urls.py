17912c2d65701c77b2c180401674ccb5f6e4d348
mauravgalik04 <malik07x24@gmail.com>
2025-04-08 18:05:23 +0530

    added structure of files

--- MoooWe/Movies/urls.py
+++ MoooWe/Movies/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.movies_home_page_view , name = 'movies_home_page')
+    path('' , views.movies_home_page_view , name = 'movies_home_page'),
+    path('/movies/' , views.movies_movies_view , name = 'movies_movies'),
+    path('/genres/' , views.movies_genre_view , name = 'movies_genre'),
+    path('/feature_movie/<int:sno>/' , views.movies_feature_movie_view , name = 'movies_feature_movie'),
+    path('/register/' , views.movies_register_view , name = 'movies_register'),
+    path('/login' , views.movies_login_view , name = 'movies_login'),
+    path('/profile/<int:id>' , views.movies_profile_view , name = 'movies_profile'),
 ]
--- MoooWe/Movies/views.py
+++ MoooWe/Movies/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Home Page view function : 
 def movies_home_page_view(request):
     return render(request , 'movies_home_page.html')
+
+
+# for displaying all the movies in a page : 
+def movies_movies_view(request):
+    return render(request , 'movies_movies.html')
+
+
+#for displaying movies in a genre wise manner : 
+def movies_genre_view(request):
+    return render(request , 'movies_genre.html')
+
+#for displaying a single movies as featured movie :
+def movies_feature_movie_view(request , sno):
+    return render(request , 'movies_feature_movie.html')
+
+#for registering a user : 
+def movies_register_view(request):
+    return render(request , 'movies_register.html')
+
+#for the user to log in:
+def movies_login_view(request):
+    return render(request , 'movies_login.html')
+
+# for the profile page :
+def movies_profile_view(request , id):
+    return render(request , 'movies_profile.html')
