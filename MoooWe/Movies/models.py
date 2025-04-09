ebba4cc0ba123769d34b04583ed2c973b8ad1ac8
mauravgalik04 <malik07x24@gmail.com>
2025-04-10 02:38:30 +0530

    made new changes and changed structure

--- MoooWe/Movies/models.py
+++ MoooWe/Movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length = 50 )
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length =20)
+    cast = models.TextField()
+    story = models.TextField()
+    imdb_rating = models.DecimalField(max_digits = 2 , decimal_places = 1)
+    your_rating = models.DecimalField(max_digits = 2 , decimal_places = 1)
+    watchlist = models.BooleanField(default = False)
+    poster = models.ImageField()
+    landscape = models.ImageField()
+    def __str__(self):
+        return self.name
--- MoooWe/Movies/urls.py
+++ MoooWe/Movies/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('' , views.movies_home_page_view , name = 'movies_home_page'),
-    path('/movies/' , views.movies_movies_view , name = 'movies_movies'),
-    path('/genres/' , views.movies_genre_view , name = 'movies_genre'),
-    path('/feature_movie/<int:sno>/' , views.movies_feature_movie_view , name = 'movies_feature_movie'),
-    path('/register/' , views.movies_register_view , name = 'movies_register'),
-    path('/login' , views.movies_login_view , name = 'movies_login'),
-    path('/profile/<int:id>' , views.movies_profile_view , name = 'movies_profile'),
+    path('movies/' , views.movies_movies_view , name = 'movies_movies'),
+    path('genres/' , views.movies_genre_view , name = 'movies_genre'),
+    path('feature_movie/<int:sno>/' , views.movies_feature_movie_view , name = 'movies_feature_movie'),
+    path('register/' , views.movies_register_view , name = 'movies_register'),
+    path('login' , views.movies_login_view , name = 'movies_login'),
+    path('profile/<int:id>' , views.movies_profile_view , name = 'movies_profile'),
 ]
